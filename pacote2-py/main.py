30cc3989e6d66532ae79004b27435b10cfa78a31
LGpicinin <lgpicinin@gmail.com>
2024-03-14 12:08:08 -0300

    resolve exercice

--- pacote2-py/main.py
+++ pacote2-py/main.py
@@ -19,22 +19,34 @@ NEGATIVO = False
 THRESHOLD = 0.8
 ALTURA_MIN = 1
 LARGURA_MIN = 1
-N_PIXELS_MIN = 1
+N_PIXELS_MIN = 400
 
 #===============================================================================
 
+class Component:
+    def __init__(self, label, n_pixels, t, l, b, r) -> None:
+        self.label = label
+        self.n_pixels = n_pixels
+        self.top = t
+        self.left = l
+        self.bottom = b
+        self.right = r
+
+
+
 def binariza (img, threshold):
+
+    rows, cols, channel = img.shape
     
-    img = np.where(img>threshold, 1, 0)
+    for row in range (rows):
+        for col in range (cols):
+            if img[row, col, 0] > threshold:
+                img[row, col, 0] = 1
+            else:
+                img[row, col, 0] = 0
 
     return img
-    ''' Binarização simples por limiarização.
 
-Parâmetros: img: imagem de entrada. Se tiver mais que 1 canal, binariza cada
-              canal independentemente.
-            threshold: limiar.
-            
-Valor de retorno: versão binarizada da img_in.'''
 
     # TODO: escreva o código desta função.
     # Dica/desafio: usando a função np.where, dá para fazer a binarização muito
@@ -42,7 +54,52 @@ Valor de retorno: versão binarizada da img_in.'''
 
 #-------------------------------------------------------------------------------
 
+def inunda (img, row, col, comp: Component, lenCol, lenRow):
+    if row < 0 | row > lenRow | col < 0 | col > lenCol:
+        return
+    
+    img[row, col, 0] = comp.label
+    comp.n_pixels = comp.n_pixels + 1
+
+    if row > comp.bottom:
+        comp.bottom = row
+    elif row < comp.top:
+        comp.top = row
+    
+    if col < comp.left:
+        comp.left = col
+    elif col > comp.right:
+        comp.right = col
+
+    if img[row+1, col, 0] == 1:
+        inunda(img, row+1, col, comp, lenCol, lenRow)
+    if img[row, col+1, 0] == 1:
+        inunda(img, row, col+1, comp, lenCol, lenRow)
+    if img[row-1, col, 0] == 1:
+        inunda(img, row-1, col, comp, lenCol, lenRow)
+    if img[row, col-1, 0] == 1:
+        inunda(img, row, col-1, comp, lenCol, lenRow)
+
 def rotula (img, largura_min, altura_min, n_pixels_min):
+
+    rows, cols, channels = img.shape
+    label = 2
+    count = 0
+    dictionary = []
+
+    for row in range (rows):
+        for col in range (cols):
+            if img[row, col, 0] == 1:
+                comp = Component(label, count, row, col, row, col)
+                inunda(img, row, col, comp, rows, cols)
+                if(comp.n_pixels > n_pixels_min):
+                    dictionary.append(comp)
+                    label = label + 1
+    
+    return dictionary
+            
+
+
     '''Rotulagem usando flood fill. Marca os objetos da imagem com os valores
 [0.1,0.2,etc].
 
@@ -87,7 +144,7 @@ def main ():
     cv2.imshow ('01 - binarizada', img)
     cv2.imwrite ('01 - binarizada.png', img*255)
 
-    '''start_time = timeit.default_timer ()
+    start_time = timeit.default_timer ()
     componentes = rotula (img, LARGURA_MIN, ALTURA_MIN, N_PIXELS_MIN)
     n_componentes = len (componentes)
     print ('Tempo: %f' % (timeit.default_timer () - start_time))
@@ -95,10 +152,10 @@ def main ():
 
     # Mostra os objetos encontrados.
     for c in componentes:
-        cv2.rectangle (img_out, (c ['L'], c ['T']), (c ['R'], c ['B']), (0,0,1))
+        cv2.rectangle (img_out, (c.left, c.top), (c.right, c.bottom), (0,0,1))
 
     cv2.imshow ('02 - out', img_out)
-    cv2.imwrite ('02 - out.png', img_out*255)'''
+    cv2.imwrite ('02 - out.png', img_out*255)
     cv2.waitKey ()
     cv2.destroyAllWindows ()
 
